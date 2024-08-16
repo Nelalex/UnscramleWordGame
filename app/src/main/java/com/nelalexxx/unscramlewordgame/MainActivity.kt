4cffb867f071c94630b2b7d41ea52b869317f84c
Nelalex <Nelalexxx@gmail.com>
2024-08-16 15:43:07 +0300

    UG-04 MainActivity

--- app/src/main/java/com/nelalexxx/unscramlewordgame/MainActivity.kt
+++ app/src/main/java/com/nelalexxx/unscramlewordgame/MainActivity.kt
@@ -5,16 +5,37 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.nelalexxx.unscramlewordgame.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.rootLayout)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val viewModel: GameViewModel = GameViewModel()
+
+        binding.nextButton.setOnClickListener {
+            val uiState: GameUiState =
+                viewModel.check(text = binding.inputFieldEditText.text.toString())
+            uiState.update(binding = binding)
+        }
+
+        binding.nextButton.setOnClickListener {
+            val uiState: GameUiState = viewModel.goNextWord()
+            uiState.update(binding = binding)
+        }
+
+
+        val uiState: GameUiState = viewModel.init()
+        uiState.update(binding = binding)
     }
+
+
 }
